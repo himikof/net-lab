da06ebfa49ba76fe1da659983bb0a7e2e45e9561
Nikita Ofitserov <himikof@gmail.com>
2013-04-16 11:40:37 +0400

    Add client.bat launcher

---
+++ client.bat
@@ -0,0 +1,7 @@
+@echo off
+set BASE=%~dp0
+set PYTHONPATH=
+cd F:\Soft\Portable Python 2.7.3.1\App
+F:
+REM "%CD%\Scripts\pip" --proxy=http://rain.ifmo.ru:3128/ install protobuf
+python.exe %BASE%\sessions-client.py
